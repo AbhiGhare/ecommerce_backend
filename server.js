6b8f757bfb966de779e7f5cb921ad142ebdd4057
AbhiGhare <ghareabhishek4@gmail.com>
2024-08-28 00:39:53 +0530

    frontend live url added

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173', // Replace with your frontend URL
+    origin: ['http://localhost:5173','https://ecommerce-frontend-asit.vercel.app'], // Replace with your frontend URL
     methods: 'GET,POST,PUT,DELETE',
     credentials: true
   }));
